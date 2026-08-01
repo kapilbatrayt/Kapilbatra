3f0eae63613fa7882d9b9f0bec55560652980e3c
kapilbatrayt <kapilbatrayt@gmail.com>
2026-08-01 20:36:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        syne: ['Syne', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
